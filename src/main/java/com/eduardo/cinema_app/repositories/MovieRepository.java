22f328685c5759ddd5a431054af43f5327367f9b
Eduardo Araujo <eduardo1996araujo@gmail.com>
2025-02-11 19:56:34 -0300

    feat: criado 2 querys para o metodo de criar a sessão

--- src/main/java/com/eduardo/cinema_app/repositories/MovieRepository.java
+++ src/main/java/com/eduardo/cinema_app/repositories/MovieRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query("SELECT m FROM Movie m WHERE " +
             "(:title IS NULL OR m.title LIKE %:title%) AND " +
             "(:genre IS NULL OR m.genre = :genre)")
     Page<Movie> findAllWithFilters(String title, Genre genre, Pageable pageable);
+
+    Optional<Movie> findByTitleIgnoreCase(String title);
 }
--- src/main/java/com/eduardo/cinema_app/repositories/RoomRepository.java
+++ src/main/java/com/eduardo/cinema_app/repositories/RoomRepository.java
@@ -3,5 +3,9 @@ package com.eduardo.cinema_app.repositories;
 import com.eduardo.cinema_app.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    Optional<Room> findByRoomNumber(Integer roomNumber);
 }
